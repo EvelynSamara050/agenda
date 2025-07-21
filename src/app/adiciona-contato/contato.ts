c19d743112ba8d1fda0c5ebf333c238ecadd5f10
Evelyn Samara da Silva <20221144010070@ifrn.local>
2025-07-21 11:07:45 -0300

    commit inicial

---
+++ src/app/adiciona-contato/contato.ts
@@ -0,0 +1,14 @@
+import { NgModel } from "@angular/forms"
+
+export class Contato {
+  nome: string
+  aniversario: Date
+  tipo: string
+
+constructor(nm: string, an:Date, tipo:string){
+  this.nome = nm 
+  this.aniversario = an
+  this.tipo = tipo
+  
+}
+}
